f73f48b62980f434d54154abff021d781e650bd5
0xpluto <jackson2020@me.com>
2023-08-30 17:06:07 -0500

    add interaction functions

--- src/components/ConnectWallet.js
+++ src/components/ConnectWallet.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import VOTE_FACTORY_ADDRESS from '../assets/addresses.js';
+import Web3 from 'web3';
 
 class ConnectWalletComponent extends Component {
     constructor(props) {
+        // Read the abi and contract address from the config file
+        let abi = require('../assets/VoteFactory.abi.json');
+
         super(props);
         this.state = {
             connected: false,
             addresses: [],
+            factory: new Web3.eth.Contract(abi, VOTE_FACTORY_ADDRESS),
+            w3: new Web3("https://api.node.glif.io")
         };
+    }
+
+    async votes() {
+        let votes = await this.state.factory.methods.deployedVotes().call();
+        return votes;
+    }
+
+    async getVoteAddress(fip) {
+        let address = await this.state.factory.methods.FIPnumToAddress(fip).call();
+        return address;
+    }
+
+    async registerVoter(voteTrackerAddress, glifPoolAddress, minerIds) {
+        let voteTracker = new this.state.w3.eth.Contract(require('../assets/VoteTracker.abi.json'), voteTrackerAddress);
+        let tx = await voteTracker.methods.registerVoter(glifPoolAddress, minerIds).send();
+        return tx;
+    }
+
+    async castVote(voteTrackerAddress, vote) {
+        let voteTracker = new this.state.w3.eth.Contract(require('../assets/VoteTracker.abi.json'), voteTrackerAddress);
+        let tx = await voteTracker.methods.castVote(vote).send();
+        return tx;
     }
 
     async connectWallet() {
